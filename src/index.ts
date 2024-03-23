4656081386d7ee9c4711300ff15d09e1a6c6ca22
Charlton Trezevant <ct@ctis.me>
2024-03-23 15:44:21 -0400

    generate etags for widgets

--- src/index.ts
+++ src/index.ts
@@ -24,20 +24,29 @@ export default {
             }
 
             if (url.pathname == '/html' || url.pathname == '/upcoming-events') {
+                const bodyText = HTMLEventWidget(eventData, request)
                 res = new Response(jsonToHTML(eventData, request));
                 res.headers.set("Content-Type", "text/html");
+                res.headers.set("Cache-Control", "public, max-age=3600");
+                res.headers.set("Etag", cyrb53(bodyText));
                 return res;
             }
 
             if (url.pathname == '/widget/next-event') {
-                res = new Response(HTMLEventWidget(eventData, request));
+                const bodyText = HTMLEventWidget(eventData, request)
+                res = new Response(bodyText);
                 res.headers.set("Content-Type", "text/html");
+                res.headers.set("Cache-Control", "public, max-age=3600");
+                res.headers.set("Etag", cyrb53(bodyText));
                 return res;
             }
 
             if (url.pathname == '/widget/carousel') {
-                res = new Response(HTMLCarouselWidget(eventData, request));
+                const bodyText = HTMLCarouselWidget(eventData, request);
+                res = new Response(bodyText);
                 res.headers.set("Content-Type", "text/html");
+                res.headers.set("Cache-Control", "public, max-age=3600");
+                res.headers.set("Etag", cyrb53(bodyText));
                 return res;
             }
 
@@ -95,6 +104,21 @@ async function parseQueryParams(url) {
     });
     return params;
 }
+
+const cyrb53 = (str, seed = 0) => {
+  let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+  for(let i = 0, ch; i < str.length; i++) {
+      ch = str.charCodeAt(i);
+      h1 = Math.imul(h1 ^ ch, 2654435761);
+      h2 = Math.imul(h2 ^ ch, 1597334677);
+  }
+  h1  = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
+  h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+  h2  = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
+  h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+
+  return (4294967296 * (2097151 & h2) + (h1 >>> 0)).toString();
+};
 
 function escapeXml(unsafeStr) {
     return unsafeStr.replace(/[<>&'"]/g, function(c) {
